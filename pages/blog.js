aff11ed6925f50fad1fd63da49f35abe06b0178a
Tomas Klimaitis <tomas@lastmile.lt>
2021-09-14 16:42:07 +0300

    first commit

---
+++ pages/blog.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Container from "../components/container";
+import Head from "next/head";
+import { Text, useColorMode, Heading, Flex, Stack } from "@chakra-ui/react";
+
+function Blog() {
+  const { colorMode } = useColorMode();
+  const colorSecondary = {
+    light: "gray.700",
+    dark: "gray.400",
+  };
+  return (
+    <>
+      <Container>
+        <Head>
+          <title>Blog</title>
+        </Head>
+        <Stack
+          as="main"
+          spacing={8}
+          justifyContent="center"
+          alignItems="flex-start"
+          m="0 auto 4rem auto"
+          maxWidth="700px"
+          px={2}
+        >
+          <Flex
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            maxWidth="700px"
+          >
+            <Heading mb={2}>Hello World</Heading>
+            <Text color={colorSecondary[colorMode]}>
+              This would be a blog.
+            </Text>
+          </Flex>
+        </Stack>
+      </Container>
+    </>
+  );
+}
+
+export default Blog;
